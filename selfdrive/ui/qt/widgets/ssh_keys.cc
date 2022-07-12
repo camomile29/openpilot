fe0b2a62c988ae379cb9799d566d995413c51fc3
kim2629 <74346292+kimjk2629@users.noreply.github.com>
2022-07-12 17:55:52 +0900

    한글화

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -4,16 +4,16 @@
 #include "selfdrive/ui/qt/api.h"
 #include "selfdrive/ui/qt/widgets/input.h"
 
-SshControl::SshControl() : ButtonControl("SSH 키", "", "경고: This grants SSH access to all public keys in your GitHub settings. Never enter a GitHub username other than your own. A comma employee will NEVER ask you to add their GitHub username.") {
+SshControl::SshControl() : ButtonControl("SSH 키", "", "경고: 이렇게 하면 GitHub 설정의 모든 공개 키에 SSH 액세스 권한이 부여됩니다. 자신의 GitHub 사용자 이름이 아닌 다른 사용자 이름을 입력하지 마십시오. 콤마 직원은 GitHub 사용자 이름을 추가하도록 요청하지 않습니다.") {
   username_label.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   username_label.setStyleSheet("color: #aaaaaa");
   hlayout->insertWidget(1, &username_label);
 
   QObject::connect(this, &ButtonControl::clicked, [=]() {
-    if (text() == "ADD") {
-      QString username = InputDialog::getText("Enter your GitHub username", this);
+    if (text() == "추가") {
+      QString username = InputDialog::getText("GitHub 아이디를 입력하세요", this);
       if (username.length() > 0) {
-        setText("LOADING");
+        setText("읽어오는중");
         setEnabled(false);
         getUserKeys(username);
       }
@@ -31,10 +31,10 @@ void SshControl::refresh() {
   QString param = QString::fromStdString(params.get("GithubSshKeys"));
   if (param.length()) {
     username_label.setText(QString::fromStdString(params.get("GithubUsername")));
-    setText("REMOVE");
+    setText("제거");
   } else {
     username_label.setText("");
-    setText("ADD");
+    setText("추가");
   }
   setEnabled(true);
 }
@@ -47,13 +47,13 @@ void SshControl::getUserKeys(const QString &username) {
         params.put("GithubUsername", username.toStdString());
         params.put("GithubSshKeys", resp.toStdString());
       } else {
-        ConfirmationDialog::alert(QString("Username '%1' has no keys on GitHub").arg(username), this);
+        ConfirmationDialog::alert(QString("Username '%1' 의 키가 GitHub에 없습니다").arg(username), this);
       }
     } else {
       if (request->timeout()) {
-        ConfirmationDialog::alert("Request timed out", this);
+        ConfirmationDialog::alert("타임아웃이 발생하였습니다", this);
       } else {
-        ConfirmationDialog::alert(QString("Username '%1' doesn't exist on GitHub").arg(username), this);
+        ConfirmationDialog::alert(QString("Username '%1' 은 GitHub의 회원이 아닙니다").arg(username), this);
       }
     }
 
