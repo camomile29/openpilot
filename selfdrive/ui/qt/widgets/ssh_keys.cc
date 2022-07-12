03958d63af4ac8d3b8d106e749a220186382e853
kim2629 <74346292+kimjk2629@users.noreply.github.com>
2022-07-12 17:17:26 +0900

    Update ssh_keys.cc

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -4,7 +4,7 @@
 #include "selfdrive/ui/qt/api.h"
 #include "selfdrive/ui/qt/widgets/input.h"
 
-SshControl::SshControl() : ButtonControl("SSH Keys", "", "Warning: This grants SSH access to all public keys in your GitHub settings. Never enter a GitHub username other than your own. A comma employee will NEVER ask you to add their GitHub username.") {
+SshControl::SshControl() : ButtonControl("SSH 키", "", "경고: This grants SSH access to all public keys in your GitHub settings. Never enter a GitHub username other than your own. A comma employee will NEVER ask you to add their GitHub username.") {
   username_label.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   username_label.setStyleSheet("color: #aaaaaa");
   hlayout->insertWidget(1, &username_label);
